8d63df761763d60a6342efc70ae7da57e3c4923e
DEJAN VOJINOVIC <DEJANVOJINOVIC@yahoo.com>
2021-01-10 15:16:54 +0100

    + ApiKey.KEY now stores key for API auth.

--- app/src/main/java/com/example/interestationapp/MainActivity.java
+++ app/src/main/java/com/example/interestationapp/MainActivity.java
@@ -1,26 +1,34 @@
 package com.example.interestationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import com.android.volley.RequestQueue;
-import com.android.volley.Request.Priority;
+import com.android.volley.Response;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
 import org.json.JSONObject;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.Toast;
-import com.android.volley.Request;
 import java.util.Date;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Queue;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     final String baseUrl = "https://interestation.azurewebsites.net/api/v1/";
+    //final String ApiKey = "isStrong(!)apiKey";
+    final int USERS = 0;
+    final int LIKES = 1;
+    final int POSTS = 2;
+
     ListView post_listView;
     List<Post> pList;
     List<User> uList;
@@ -40,113 +48,107 @@ public class MainActivity extends AppCompatActivity {
         lList = new ArrayList<>();
         requestQueue = Volley.newRequestQueue(this);//This will take care of background newtwork activities
 
-        getUsers();
-        getLikes();
-        getPosts();
+        fetchData(USERS);
+        fetchData(LIKES);
+        fetchData(POSTS);
 
         postAdapter = new PostAdapter(this, R.layout.customcell, pList, requestQueue);
         post_listView.setAdapter(postAdapter);
     }
 
-    private void getUsers() {
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, baseUrl + "Users", null, response -> {
+    public  void fetchData(int object){
+        String[] ext = new String[]{"Users", "Likes", "Posts"};
+
+        JsonArrayRequest request = new JsonArrayRequest(baseUrl + ext[object], response -> {
             try {
                 for (int i = 0; i < response.length(); i++) {
                     JSONObject jsonObject = response.getJSONObject(i);
 
-                    String id = jsonObject.getString("id");
-                    String firstName = jsonObject.getString("firstName");
-                    String lastName = jsonObject.getString("lastName");
-                    String username = jsonObject.getString("userName");
-                    String image = jsonObject.getString("profilePic");
-
-                    uList.add(new User(id, firstName, lastName, username, image));
-
-                    for (Post p : pList) {
-                        if (id.equals(p.ownerId)) {
-                            p.ownerNick = username;
-                            p.ownerImg = image;
-                        }
+                    if(object == 0){
+                        unpackUsers(jsonObject);
+                    }else if(object == 1) {
+                        unpackLikes(jsonObject);
+                    }else {
+                        unpackPosts(jsonObject);
                     }
                 }
-                postAdapter.notifyDataSetChanged();//To prevent app from crashing when updating
-                //UI through background Thread
+                postAdapter.notifyDataSetChanged();//To prevent app from crashing when updating UI through background Thread
             } catch (Exception w) {
                 Toast.makeText(MainActivity.this, w.getMessage(), Toast.LENGTH_LONG).show();
             }
-        }, error -> Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show());
-        requestQueue.add(jsonArrayRequest);
+        }, errorListener){
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<>();
+                params.put("ApiKey", ApiKey.KEY);
+
+                return params;
+            }
+        };
+        requestQueue.add(request);
     }
 
-    private void getPosts() {
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, baseUrl + "Posts", null, response -> {
-            try {
-                for (int i = 0; i < response.length(); i++) {
-                    JSONObject jsonObject = response.getJSONObject(i);
+    private final Response.ErrorListener errorListener = error -> Log.d("REST error", error.getMessage());
 
-                    int id = jsonObject.getInt("postId");
-                    String ownerId = jsonObject.getString("ownerId");
-                    String dateString = jsonObject.getString("dateCreated");
-                    String text = jsonObject.getString("text");
-                    String image = jsonObject.getString("image");
+    public void unpackUsers(JSONObject jsonObject) throws JSONException {
+        String id = jsonObject.getString("id");
+        String firstName = jsonObject.getString("firstName");
+        String lastName = jsonObject.getString("lastName");
+        String username = jsonObject.getString("userName");
+        String image = jsonObject.getString("profilePic");
 
-                    String ownerNick = "username";
-                    String ownerImg = null;
+        uList.add(new User(id, firstName, lastName, username, image));
 
-                    for (User u : uList) {
-                        if (u.id.equals(ownerId)) {
-                            ownerNick = u.userName;
-                            ownerImg = u.image;
-                        }
-                    }
-
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-                    Date date = simpleDateFormat.parse(dateString);
-
-                    Post p = new Post(id, ownerId, ownerNick, ownerImg, text, date, image);
-                    pList.add(0, p);
-
-                    for (Like l: lList){
-                        if(l.postId == id){
-                            p.likes.add(l);
-                        }
-                    }
-                }
-                postAdapter.notifyDataSetChanged();//To prevent app from crashing when updating
-                //UI through background Thread
-
-            } catch (Exception w) {
-                Toast.makeText(MainActivity.this, w.getMessage(), Toast.LENGTH_LONG).show();
+        for (Post p : pList) {
+            if (id.equals(p.ownerId)) {
+                p.ownerNick = username;
+                p.ownerImg = image;
             }
-        }, error -> Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show());
-        requestQueue.add(jsonArrayRequest);
+        }
     }
 
-    private void getLikes() {
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, baseUrl + "Likes", null, response -> {
-            try {
-                for (int i = 0; i < response.length(); i++) {
-                    JSONObject jsonObject = response.getJSONObject(i);
+    public void unpackLikes(JSONObject jsonObject) throws JSONException {
+        String id = jsonObject.getString("likeId");
+        String userId = jsonObject.getString("userId");
+        int postId = jsonObject.getInt("postId");
 
-                    String id = jsonObject.getString("likeId");
-                    String userId = jsonObject.getString("userId");
-                    int postId = jsonObject.getInt("postId");
+        Like l = new Like(id, userId, postId);
+        lList.add(l);
 
-                    Like l = new Like(id, userId, postId);
-                    lList.add(l);
-
-                    for(Post p: pList){
-                        if(p.id == postId){
-                            p.likes.add(l);
-                        }
-                    }
-                }
-                postAdapter.notifyDataSetChanged();//To prevent app from crashing when updating
-                //UI through background Thread
-            } catch (Exception w) {
-                Toast.makeText(MainActivity.this, w.getMessage(), Toast.LENGTH_LONG).show();
+        for(Post p: pList){
+            if(p.id == postId){
+                p.likes.add(l);
             }
-        }, error -> Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show());
-        requestQueue.add(jsonArrayRequest);
+        }
+    }
+
+    public void unpackPosts(JSONObject jsonObject) throws JSONException, ParseException {
+        int id = jsonObject.getInt("postId");
+        String ownerId = jsonObject.getString("ownerId");
+        String dateString = jsonObject.getString("dateCreated");
+        String text = jsonObject.getString("text");
+        String image = jsonObject.getString("image");
+
+        String ownerNick = "username";
+        String ownerImg = null;
+
+        for (User u : uList) {
+            if (u.id.equals(ownerId)) {
+                ownerNick = u.userName;
+                ownerImg = u.image;
+            }
+        }
+
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        Date date = simpleDateFormat.parse(dateString);
+
+        Post p = new Post(id, ownerId, ownerNick, ownerImg, text, date, image);
+        pList.add(0, p);
+
+        for (Like l : lList) {
+            if (l.postId == id) {
+                p.likes.add(l);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/interestationapp/PostAdapter.java
+++ app/src/main/java/com/example/interestationapp/PostAdapter.java
@@ -1,6 +1,8 @@
 package com.example.interestationapp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,21 +14,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import com.android.volley.AuthFailureError;
+import androidx.annotation.RequiresApi;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
 import com.squareup.picasso.Picasso;
-//import com.squareup.picasso.Request;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class PostAdapter extends ArrayAdapter<Post> {
@@ -45,11 +46,12 @@ public class PostAdapter extends ArrayAdapter<Post> {
         this.requestQueue = requestQueue;
     }
 
+    @SuppressLint("SetTextI18n")
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(resource,null,false);
+        @SuppressLint("ViewHolder") View view = inflater.inflate(resource,null,false);
         TextView owner = view.findViewById(R.id.owner_name_lbl);
         TextView date = view.findViewById(R.id.date_lbl);
         TextView text = view.findViewById(R.id.post_text_lbl);
@@ -61,74 +63,68 @@ public class PostAdapter extends ArrayAdapter<Post> {
 
         Post post = postList.get(position);
 
-        likeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String testLikeGuid = UUID.randomUUID().toString(); //generate random GUID
+        likeBtn.setOnClickListener(v -> {
+            String testLikeGuid = UUID.randomUUID().toString(); //generate random GUID
 
-                //send POST request (create new like)
-                try{
-                    JSONObject jsonObject = new JSONObject();
-                    jsonObject.put("likeId", testLikeGuid);
-                    jsonObject.put("userId", post.ownerId);
-                    jsonObject.put("user", JSONObject.NULL);
-                    jsonObject.put("postId", post.id);
-                    jsonObject.put("post", JSONObject.NULL);
+            //send POST request (create new like)
+            try{
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("likeId", testLikeGuid);
+                jsonObject.put("userId", post.ownerId);
+                jsonObject.put("user", JSONObject.NULL);
+                jsonObject.put("postId", post.id);
+                jsonObject.put("post", JSONObject.NULL);
 
-                    final String mRequestBody = jsonObject.toString();
+                post.likes.add(new Like(testLikeGuid, post.ownerId, post.id));
+                likesCount.setText(post.likes.size() + "");
 
-                    String url = "https://interestation.azurewebsites.net/api/v1/Likes";
-                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            Log.i("LOG_VOLLEY", response);
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Log.e("LOG_VOLLEY", error.toString());
-                        }
+                final String mRequestBody = jsonObject.toString();
+
+                String url = "https://interestation.azurewebsites.net/api/v1/Likes";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url, response -> Log.i("LOG_VOLLEY", response), error -> Log.e("LOG_VOLLEY", error.toString())
+                ){
+                    @Override
+                    public String getBodyContentType() {
+                        return "application/json; charset=utf-8";
                     }
-                    ){
-                        @Override
-                        public String getBodyContentType() {
-                            return "application/json; charset=utf-8";
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public byte[] getBody() {
+                        return mRequestBody.getBytes(StandardCharsets.UTF_8);
+                    }
+                    @Override
+                    protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                        String responseString = "";
+                        if (response != null) {
+                            responseString = String.valueOf(response.statusCode);
                         }
-                        @Override
-                        public byte[] getBody() throws AuthFailureError {
-                            try {
-                                return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
-                            } catch (UnsupportedEncodingException uee) {
-                                VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
-                                return null;
-                            }
-                        }
-                        @Override
-                        protected Response<String> parseNetworkResponse(NetworkResponse response) {
-                            String responseString = "";
-                            if (response != null) {
-                                responseString = String.valueOf(response.statusCode);
-                            }
-                            return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
-                        }
-                    };
+                        assert response != null;
+                        return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                    }
+
+                    @Override
+                    public Map<String, String> getHeaders() {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("ApiKey", ApiKey.KEY);
+                        params.put("Content-Type", "application/JSON");
+                        return params;
+                    }
+                };
 
 
-                    requestQueue.add(stringRequest);
+                requestQueue.add(stringRequest);
 
-                } catch (JSONException e){
-                    e.printStackTrace();
-                }
-                Toast.makeText(getContext(), "Liked post by " + post.ownerNick, Toast.LENGTH_SHORT).show();
+            } catch (JSONException e){
+                e.printStackTrace();
             }
+            Toast.makeText(getContext(), "Liked post by " + post.ownerNick, Toast.LENGTH_SHORT).show();
         });
 
         owner.setText(post.ownerNick);
         date.setText(post.date.toString());
         text.setText(post.text);
 
-        String likeNumDisplay = post.likes.size() + "";
-        likesCount.setText(likeNumDisplay);
+        likesCount.setText(post.likes.size() + "");
 
         if(post.image.equals("null")){
             image.setVisibility(View.GONE);
